be32daba0a6f0f114b5fbfd93ebf8627ef7ce0f4
blaycoder <onatola4real@gmail.com>
2022-09-21 21:53:36 -0700

    added a new file

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# kill a process using pkill with puppet
+# execute pkill command to kill bashscript file killmenow
+exec {
+     'pkill':
+     command => 'pkill -f killmenow',
+     path    => '/usr/bin',
+     }
